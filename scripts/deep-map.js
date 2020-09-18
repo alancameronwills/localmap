732e6bb1a13e9e33827ec0e40e971188f8b55ac8
Alan Cameron Wills <alan@cameronwills.org>
2020-09-18 20:52:14 +0100

    debug

--- scripts/deep-map.js
+++ scripts/deep-map.js
@@ -235,6 +235,7 @@ function dropSplash() {
 /** Listen for messages from parent if we're in an iFrame */
 function setParentListener() {
     window.addEventListener("message", function (event) {
+        alert("message " + event.data);
         if (event.source == window.parent && event.data.op == "gotoPlace") {
             onPauseButton(true); // Stop tracking GPS
             g("splash").style.display = "none"; // Spash screen
