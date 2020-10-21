9180f5548470cb57ab4903bd22d31baf90acd65f
VoidedRL <49625122+VoidedRL@users.noreply.github.com>
2020-10-21 17:23:36 +0100

    Service worker integration

---
+++ scripts/service-worker.js
@@ -0,0 +1,25 @@
+self.addEventListener('install', function(e) {
+    e.waitUntil(
+      caches.open('airhorner').then(function(cache) {
+        return cache.addAll([
+          '/localmap/scripts/index.html',
+          '/localmap/scripts/deep-map.css',
+          '/localmap/scripts/deep-map.js',
+        ]);
+      })
+    );
+   });
+
+   self.addEventListener('fetch', function(event) {
+    console.log(event.request.url);
+   });
+
+   self.addEventListener('fetch', function(event) {
+    console.log(event.request.url);
+   
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+        return response || fetch(event.request);
+      })
+    );
+   });
