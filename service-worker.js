faaa6d5bb95c1670302d2aeb942e5b3540836b9d
VoidedRL <49625122+VoidedRL@users.noreply.github.com>
2020-10-27 17:02:36 +0000

    Directory fixed

--- service-worker.js
+++ service-worker.js
@@ -2,8 +2,8 @@ self.addEventListener('install', function(e) {
     e.waitUntil(
       caches.open('deep-map').then(function(cache) {
         return cache.addAll([
-          '/localmap/scripts/index.html',
-          '/localmap/scripts/deep-map.css',
+          '/localmap/index.html',
+          '/localmap/css/deep-map.css',
           '/localmap/scripts/deep-map.js',
         ]);
       })
