fa497455d2c2323cc66db0639424d99b5de9b947
VoidedRL <49625122+VoidedRL@users.noreply.github.com>
2020-10-27 18:36:56 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -5,17 +5,23 @@ self.addEventListener('install', function(e) {
           '/localmap/index.html',
           '/localmap/css/deep-map.css',
           '/localmap/scripts/deep-map.js',
+          '/localmap/scripts/util.js'
         ]);
       })
     );
    });
 
-   self.addEventListener('fetch', function(event) {
-    console.log(event.request.url);
-   
-    event.respondWith(
-      caches.match(event.request).then(function(response) {
-        return response || fetch(event.request);
+   self.addEventListener('fetch', (e) => {
+    e.respondWith(
+      caches.match(e.request).then((r) => {
+            console.log('[Service Worker] Fetching resource: '+e.request.url);
+        return r || fetch(e.request).then((response) => {
+                  return caches.open('deep-map').then((cache) => {
+            console.log('[Service Worker] Caching new resource: '+e.request.url);
+            cache.put(e.request, response.clone());
+            return response;
+          });
+        });
       })
     );
-   });
+  });
