1ea15ba303d94a913489751c152685c079c99ce4
Alan Cameron Wills <alan@cameronwills.org>
2021-01-09 11:38:37 +0000

    fix

--- scripts/sign-in.js
+++ scripts/sign-in.js
@@ -18,9 +18,7 @@ function onClickSignIn() {
 // Called from signinDialog
 function signin(nobreakout) {
     
-    var isSafari = /constructor/i.test(window.HTMLElement) 
-        || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] 
-        || (typeof safari !== 'undefined' && window['safari'].pushNotification));
+    var isSafari = /Apple/i.test(navigator.vendor);
 
     if (isSafari && window.location != window.parent.location && !nobreakout) {
         // We're in a frame in Safari. Sign-in won't work properly.
