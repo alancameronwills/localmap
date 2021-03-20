00aad6ffa0f17b27cf5aae2199c3c5c7b4213a53
Alan Cameron Wills <alan@cameronwills.org>
2021-03-20 13:51:51 +0000

    fix

--- scripts/deep-map.js
+++ scripts/deep-map.js
@@ -398,7 +398,7 @@ function stopIncrementalUpdate() {
  */
 function showPic(pic, pin, runShow, autozoom = true, fromClick = false) {
     closePopup(true);
-    //if (pin.place && pin.place.group) index.expandToGroup(pin.place.group);
+    if (pin.place && pin.place.group) index.expandToGroup(pin.place.group);
     if (fromClick || !(pic && pic.isPicture)) window.lightboxU.unexpand();
     if (pic && !pic.isPicture && !pic.embed) {
         // pic is actually a PDF or some other sort of file
