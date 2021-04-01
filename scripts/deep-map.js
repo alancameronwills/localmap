9a0fdea1edd90e6b722e86937dcfd7cc566d1ba4
Geraintps <geraintps@gmail.com>
2021-04-01 15:56:29 +0100

    Update deep-map.js

--- scripts/deep-map.js
+++ scripts/deep-map.js
@@ -1004,28 +1004,8 @@ function selectCartography() {
 }
 var selectedMap;
 function mapSelect() {
-    var params = window.location.search;
-    var paramsIncludeProject = params.includes("project");
-    var paramsIncludeCartography = params.includes("cartography")
-    if (paramsIncludeProject == true && paramsIncludeCartography == true) { 
-        if (params.includes("google")) {
-            var newParams = params.replace("google", selectedMap);
-        }
-        else if (params.includes("bing")) {
-            var newParams = params.replace("bing", selectedMap);
-        }
-        else if (params.includes("osm")) {
-            var newParams = params.replace("osm", selectedMap);
-        }
-        window.location.search = newParams;
-    } else if (paramsIncludeProject == true && paramsIncludeCartography == false){
-        window.location.search = params + "&cartography=" + selectedMap;
-    } else {
-        window.location.search = "?cartography=" + selectedMap;
-    }
-    
+    window.location.search = "?cartography=" + selectedMap + "&project=" + (window.project.id);
 }
-
 function opacitySlider() {
     map.pinOpacity.Value = (map.pinOpacity.Value + 1) % 4;
 
