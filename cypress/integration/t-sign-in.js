d1414a8e1839c3acdbc29b371a8fdd15e3b79da5
Alan Cameron Wills <alan@cameronwills.org>
2021-04-06 02:05:05 +0100

    sign-in tests

--- cypress/integration/t-sign-in.js
+++ cypress/integration/t-sign-in.js
@@ -4,6 +4,27 @@ describe("Sign in tests", function () {
         cy.get("#continueButton", { timeout: 30000 }).then(b=>{b.click();});
         cy.wait(2000);
     });
-    
-  
+
+    it("Can add a place", function () {
+        cy.visit(this.site + `?project=${this.TestProjectId}`);
+        cy.get("#continueButton", { timeout: 30000 }).then(b=>{b.click();});
+        cy.get(".gm-svpc", { timeout: 30000 }); // Google up
+        cy.get("#indexSidebar").contains("Modern meridian").should("be.visible");
+        cy.get(".indexPlaceContainer").should("have.length", 1);
+        cy.get("#addressSearchBox").type("SE10 8XJ\n");
+        cy.get('#addPlaceButton').click();
+        cy.get('#petri').click();
+        cy.get('#popuptext').type("Test item 1").should('have.text', "Test item 1");
+        cy.get("#popclose").click();
+        cy.get("#popup").should("not.be.visible");
+        cy.get(".indexPlaceContainer").should("have.length", 2);
+        cy.get("#indexSidebar").contains("Test item 1").should("be.visible");
+        
+        // Check it's still there when we refresh:
+        cy.visit(this.site + `?project=${this.TestProjectId}`);
+        cy.get(".gm-svpc", { timeout: 30000 }); // Google up
+        cy.get(".indexPlaceContainer").should("have.length", 2);
+        cy.get("#indexSidebar").contains("Test item 1").should("be.visible");
+
+    })
 })
---
+++ cypress/support/index.js
@@ -0,0 +1,32 @@
+// ***********************************************************
+// This example support/index.js is processed and
+// loaded automatically before your test files.
+//
+// This is a great place to put global configuration and
+// behavior that modifies Cypress.
+//
+// You can change the location of this file or turn off
+// automatically serving support files with the
+// 'supportFile' configuration option.
+//
+// You can read more here:
+// https://on.cypress.io/configuration
+// ***********************************************************
+
+// Import commands.js using ES2015 syntax:
+import './commands'
+
+// Alternatively you can use CommonJS syntax:
+// require('./commands')
+
+before(() => {
+  // Remove all but one test place:
+  cy.request("https://deep-map.azurewebsites.net/api/deleteTestPlaces?code=se3mQ/fs2Nz8elrtKyXb4VggJnUycdMbPdislVJ1ekKoz0Tf5OyrUA==");
+});
+
+beforeEach(() => {
+  // Put {"site":"local"} or ..."live"} in cypress.env.json 
+  cy.wrap(Cypress.env('site') == "local" ? Cypress.env("localRoot") : Cypress.env("liveRoot")).as("site");
+  cy.wrap(Cypress.env('TestProjectId')).as("TestProjectId");
+  // See https://docs.cypress.io/guides/core-concepts/variables-and-aliases#Sharing-Context
+});
