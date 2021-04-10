a34f13a5f2013e84c73ccec5abe003c65f0f7985
Alan Cameron Wills <alan@cameronwills.org>
2021-04-10 18:06:56 +0100

    Tidy tests

--- cypress/integration/t4-links-to-places.js
+++ cypress/integration/t4-links-to-places.js
@@ -2,39 +2,33 @@ import { MapTest } from "../bits/MapTest.js";
 
 describe("Links to places", function () {
 
-    // IMHO this is more readable than .as():
-    let link = null;
     it("Can get sharing link and use it", function () {
         let mapTest = new MapTest(this);
         mapTest.addPlaceAtPostcode("SE10 9NN", (editor) => {
             editor.textInput("Test place", "ego");
             cy.get("#getLinkButton").click();
             cy.get("#msgbox").then($m => {
-                link = $m.val();
+                cy.wrap($m.val()).as("placeLink");
                 cy.get("#message").click(1, 1); // close msg
             })
-        });
-        // Wait for editor to finish before refreshing:
-        mapTest.indexContains("Test place", 2).then(() => {
+        // Perform next bit in then() because we're using the alias 'link' 
+        }).then(() => {
             // Re-open window on shared place link:
-            mapTest.visit(link).then(() => {
-                cy.get(".infoBox").should("have.text", "Test place");
-            });
-        })
+            mapTest.visit(this.placeLink);
+            cy.get(".infoBox").should("have.text", "Test place");
+        });
     });
 
     // Depends on wired-in RowKey of Modern Meridian place:
     it("Can find place from iframe API", function () {
         let mapTest = new MapTest(this);
         // Open one place and check content, but not for editing:
-      //  mapTest.openEditorViaAPI('8dwn40fvv2%7C320501040707199024165', 1, "Modern meridian", true)
-       // .then(() =>{
-                // Open another place and check content, then delete:
-                let testId = link.replace(/^.*=/, "");
-                mapTest.openEditorViaAPI(testId, 0, "Test place", false, editor => {
-                    editor.textInput("{del}");
-                });
-         //   });
+        mapTest.openEditorViaAPI('8dwn40fvv2%7C320501040707199024165', 1, "Modern meridian", true)
+        // Open another place and check content, then delete:
+        let testId = this.placeLink.replace(/^.*=/, "");
+        mapTest.openEditorViaAPI(testId, 0, "Test place", false, editor => {
+            editor.textInput("{del}");
+        });
     });
 
 
