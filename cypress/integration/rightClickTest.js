588763c9621e7a11f1d345dd8d9d478ba6033159
Geraintps <geraintps@gmail.com>
2021-04-19 02:10:51 +0100

    Update rightClickTest.js

--- cypress/integration/rightClickTest.js
+++ cypress/integration/rightClickTest.js
@@ -1,18 +1,17 @@
-describe("Right Click Test", () => { 
-    //let site = "https://deep-map.azurewebsites.net";
-    let site = Cypress.env('site') == "local" ? Cypress.env("localRoot") : Cypress.env("liveRoot"); 
-    // put {"site":"local"} or ..."live"} in cypress.env.json
+import { MapTest } from "../bits/MapTest.js";
 
-    it("Load Google Map and check right click menu", () => {
-        cy.visit(site+"/?project=folio");
-        cy.get("#continueButton", { timeout: 30000 }).then(b=>{b.click();});
-        cy.get(".gm-svpc", { timeout: 30000 }); // Google up
-        // Index click fails
-        cy.get('.groupHead[title="Streets"]', { timeout: 8000 }).should("be.visible").click();
-        //cy.get("#sub\\#Streets ").should("be.visible");
+
+describe("Right Click Test", function() {
+
+    it("Load Google Map and check right click menu", function() {
+        let mapTest = new MapTest(this);
+        mapTest.indexContains("Modern meridian", 1);
+        cy.wait(1000);
         cy.get('#theMap').rightclick();       // Right click on the map
-        cy.pause();
+        cy.get('.gm-style-iw').should('be.visible');
+        cy.wait(1000);
         cy.get('#theMap').click();
+        cy.get('.gm-style-iw').should('not.exist');
     });
 
 })
